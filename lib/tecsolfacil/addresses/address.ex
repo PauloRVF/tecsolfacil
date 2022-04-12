57199b34324982b8f4edf09c01aab070a524df5f
valvazoripaulo <valvazori.paulo@gmail.com>
2022-04-12 20:10:25 -0300

    add cep sanitization to ensure only numbers in persistence

--- lib/tecsolfacil/addresses/address.ex
+++ lib/tecsolfacil/addresses/address.ex
@@ -27,9 +27,20 @@ defmodule Tecsolfacil.Addresses.Address do
   defp validate_cep(changeset) do
     changeset
     |> validate_required([:cep])
-    |> validate_length(:cep, is: 8, message: "The field CEP must have 8 digits")
-    |> validate_format(:cep, ~r/[\d]+$/, message: "Only allowed numbers for CEP")
+    |> sanitize_cep()
+    |> validate_length(:cep, is: 8, message: "The field CEP must have 8 numeric digits")
     |> unsafe_validate_unique(:cep, Tecsolfacil.Repo)
     |> unique_constraint(:cep)
+  end
+
+  defp sanitize_cep(changeset) do
+    cep = get_change(changeset, :cep)
+
+    if cep do
+      changeset
+      |> put_change(:cep, String.replace(cep, ~r/[^\d]/, ""))
+    else
+      changeset
+    end
   end
 end
