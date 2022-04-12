55e9b440743a85a8895ea56e0470bf369deeec38
valvazoripaulo <valvazori.paulo@gmail.com>
2022-04-12 20:55:47 -0300

    test mocking controller test and expand expected behaviours

--- test/tecsolfacil_web/api/v1/controllers/address_controller_test.exs
+++ test/tecsolfacil_web/api/v1/controllers/address_controller_test.exs
@@ -3,11 +3,13 @@ defmodule TecsolfacilWeb.Api.V1.AddressControllerTest do
   use TecsolfacilWeb.ConnCase, async: true
   alias Tecsolfacil.Addresses
   alias Tecsolfacil.Addresses.Address
+  alias Tecsolfacil.CepWs.Adapters.Mock, as: CepWsMock
+  alias Tecsolfacil.CepWs.Client
 
   @valid_address %{cep: "00000000", logradouro: "rua teste, 1"}
 
   describe "show/2" do
-    test "when is a valid cep", %{conn: conn} do
+    test "when cep exists in database", %{conn: conn} do
       Addresses.create(@valid_address)
 
       conn = get(conn, Routes.address_path(conn, :show, "00000000"))
@@ -17,11 +19,52 @@ defmodule TecsolfacilWeb.Api.V1.AddressControllerTest do
       assert response_body["logradouro"] == "rua teste, 1"
     end
 
-    test "when is not a valid cep", %{conn: conn} do
+    test "when cep not exists in database but exists on WS", %{conn: conn} do
+      expected_body = %{
+        "bairro" => "bairro",
+        "cep" => "00000-000",
+        "complemento" => "",
+        "ddd" => "00",
+        "gia" => "0000",
+        "ibge" => "0000000",
+        "localidade" => "cidade",
+        "logradouro" => "Rua teste",
+        "siafi" => "0000",
+        "uf" => "UF"
+      }
+      Hammox.expect(CepWsMock, :request, fn _method, _endpoint ->
+        {:ok, %{status: 200, body: expected_body}}
+      end)
+
+      conn = get(conn, Routes.address_path(conn, :show, "00000000"))
+
+      assert response_body = json_response(conn, 200)
+      assert response_body["cep"] == "00000000"
+      assert response_body["logradouro"] == "Rua teste"
+    end
+
+    test "when cep don't exist in neither search sources", %{conn: conn} do
+      expected_body = %{"erro" => "true"}
+      Hammox.expect(CepWsMock, :request, fn _method, _endpoint ->
+        {:error, :not_found}
+      end)
+
       conn = get(conn, Routes.address_path(conn, :show, "00000000"))
 
       assert response_body = json_response(conn, 404)
       assert response_body == %{"error" => %{"detail" => "cep 00000000 not found"}}
+    end
+
+    test "when is a badly formatted cep", %{conn: conn} do
+      expected_body = %{"erro" => "true"}
+      Hammox.expect(CepWsMock, :request, fn _method, _endpoint ->
+        {:error, :bad_request}
+      end)
+
+      conn = get(conn, Routes.address_path(conn, :show, "AAAAAAA"))
+
+      assert response_body = json_response(conn, 400)
+      assert response_body == %{"error" => %{"detail" => "bad request"}}
     end
   end
 end
