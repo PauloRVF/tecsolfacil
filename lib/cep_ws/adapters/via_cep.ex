4e14859f1927bca8c74352dbbf4286fa016f4415
valvazoripaulo <valvazori.paulo@gmail.com>
2022-04-12 18:20:54 -0300

    add cep ws adapter for via cep

---
+++ lib/cep_ws/adapters/via_cep.ex
@@ -0,0 +1,28 @@
+defmodule Tecsolfacil.CepWs.Adapters.ViaCep do
+  alias Plug.Conn.Status
+
+  @behaviour Tecsolfacil.CepWs.Client
+  @base_url "https://viacep.com.br/ws/"
+
+  def request(method, resource) do
+    method
+    |> Finch.build(@base_url <> resource <> "/json")
+    |> Finch.request(:finch)
+    |> handle_result()
+  end
+
+  defp handle_result({:ok, %{status: 200, body: body}}) do
+    body = Jason.decode!(body)
+    IO.inspect(body)
+    if Map.get(body, "erro") do
+      {:error, %{body: body, status: 404}}
+    else
+      {:ok, %{body: body, status: 200}}
+    end
+  end
+
+  defp handle_result({:ok, %{status: status}}),
+    do: {:error, %{reason: Status.reason_phrase(status), status: status}}
+
+  defp handle_result({:error, reason}), do: {:error, %{reason: inspect(reason)}}
+end
